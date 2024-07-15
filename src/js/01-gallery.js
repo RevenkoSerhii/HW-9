ad94143d41046a117d133efc60aeadfddfe025a4
Revenko Serhii <revenko83@gmail.com>
2024-07-15 18:53:36 +0300

    fix simpelightbox murcap

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+
+makeItemGallery(galleryItems);
+function makeItemGallery (galleryItems){
+    const items = galleryItems.map(({original, preview, description}) => 
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" 
+        alt="${description}" />
+        </a>
+        </li>`
+    ).join("");
+    
+    gallery.innerHTML = items;
+};
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+  console.log(lightbox)
+  
 console.log(galleryItems);
