3566946d66e0a8e01b1b0ff67ab7fcd071aa41a6
Revenko Serhii <revenko83@gmail.com>
2024-07-23 21:54:38 +0300

    HM9 finished

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector(".feedback-form");
+const getedItem = localStorage.getItem("feedback-form-state");
+if (getedItem) {
+    const { email, message } = JSON.parse(getedItem);
+    form.elements.email.value = email || '';
+    form.elements.message.value = message || '';
+}
+
+
+form.addEventListener("input",throttle(onInput, 500) );
+function onInput(){
+    const email = form.elements.email.value;
+    const message = form.elements.message.value;
+    const formValues = {email, message};
+    
+    localStorage.setItem("feedback-form-state", JSON.stringify(formValues)); 
+}
+
+
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    const email = form.elements.email.value;
+    const message = form.elements.message.value;
+    const formValues = { email, message };
+    
+    console.log(formValues);
+
+    localStorage.removeItem("feedback-form-state");
+    form.reset();
+    // console.log(localStorage.getItem("feedback-form-state"))
+    // localStorage.removeItem("feedback-form-state");
+    // form.reset();
+  });
